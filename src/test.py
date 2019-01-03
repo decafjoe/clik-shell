624b1b2ea4002598f15e7d265af7528e20b468fb
Joe Joyce <joe@decafjoe.com>
2019-01-02 21:24:38 -0500

    Updates copyright year to 2019.

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@
 Tests for :mod:`clik_shell`.
 
 :author: Joe Joyce <joe@decafjoe.com>
-:copyright: Copyright (c) Joe Joyce and contributors, 2017-2018.
+:copyright: Copyright (c) Joe Joyce and contributors, 2017-2019.
 :license: BSD
 """
 import clik_shell  # noqa: F401 (required for coverage to generate report)
